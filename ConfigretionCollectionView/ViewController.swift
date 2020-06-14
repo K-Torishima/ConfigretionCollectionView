628c44e5824c6aa0f81442b31bbb0e9a18e5d843
koji-torishima <torikou3685@gmail.com>
2020-06-15 02:27:36 +0900

    Initial Commit

---
+++ ConfigretionCollectionView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ConfigretionCollectionView
+//
+//  Created by koji torishima on 2020/06/15.
+//  Copyright © 2020 koji torishima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
